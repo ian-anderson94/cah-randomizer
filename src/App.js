acfa260984d790e21b35dd118f971ce2cc91601b
Ian <ian@Ians-MacBook-Air.local>
2022-06-18 13:50:37 -1000

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+
+import cards from './resources/black_cards.txt'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cardMap, setCardMap] = useState(new Map())
+    const [prompt, setPrompt] = useState("Click button to start!")
+
+    async function initCardMap() {
+        await fetch(cards)
+            .then(r => r.text())
+            .then(text => {
+                const PROMPT_ID = 0
+                const PROMPT_TEXT = 3
+
+                var cards = new Map()
+                var lines = text.split('\n')
+
+                lines.forEach(line => {
+                    var columns = line.split('\t')
+                    cards.set(parseInt(columns[PROMPT_ID]), columns[PROMPT_TEXT])
+                })
+
+                setCardMap(cards)
+            }
+        );
+    }
+
+    function getRandomPromptFromMap(e) {
+        var num
+
+        while (!cardMap.has(num)) {
+            num = Math.floor(Math.random() * cardMap.size)
+        }
+
+        setPrompt(cardMap.get(num))
+    }
+
+    useEffect(() => { initCardMap() }, []);
+
+    return (
+        <div className='main-parent'>
+            <div className='card-prompt'>{prompt}</div>
+            <div className='randomize-button'>
+                <button onClick={getRandomPromptFromMap}>Get new card</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
